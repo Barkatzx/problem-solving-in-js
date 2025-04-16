7bad294a4d1249e12dc5d3475f9fe5537f3925fb
Barkat Ullah <barkatullah.zx@gmail.com>
2025-04-16 15:06:03 +0600

    Problem Reverse String

--- reverse-string.js
+++ reverse-string.js
@@ -1,1 +1,6 @@
 // Given a string (e.g., "hello (olleh)"), write a JavaScript function to reverse the string.
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+console.log(reverseString('Hello'));
